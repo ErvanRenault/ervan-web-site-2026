cb00d27d34135f6e97816ded9119b3fdbc4050b7
ErvanRenault <ervanrenault@gmail.com>
2026-02-08 23:26:40 +0100

    Add custome component

--- src/app/components/info-container/info-container.ts
+++ src/app/components/info-container/info-container.ts
@@ -1,11 +1,17 @@
-import { Component } from '@angular/core';
+import {Component, Input, TemplateRef} from '@angular/core';
+import {NgTemplateOutlet} from '@angular/common';
 
 @Component({
   selector: 'app-info-container',
-  imports: [],
+  imports: [
+    NgTemplateOutlet
+  ],
   templateUrl: './info-container.html',
   styleUrl: './info-container.scss',
 })
 export class InfoContainer {
+
+  @Input() content!: TemplateRef<any>;
+
 
 }
